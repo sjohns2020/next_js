380e7389ca03b6a0451fe0b3cd5668f3fbbddd60
Sean Johnson <98856218+sjohns2020@users.noreply.github.com>
2023-07-21 15:46:37 +0100

    multi page form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
 
 import './App.css';
 import pic from './pic.webp'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    email: ""
+  })
+
+  const hangleFormData = (e) => {
+    const formInputData = {...formData}
+    formInputData[e.target.name] = e.target.value
+    setFormData(formInputData)
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    setCurrentPage(currentPage + 1)
+  }
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1)
+  }
+
+
+
+
   return (
     <div className="App">
-      <h1>Next JS project going here</h1>
-      <img src={pic} alt="" />
+      <h1>Multi Step Form</h1>
+      <form onSubmit={handleFormSubmit}>
+
+        { currentPage === 1 ? 
+        <div>
+        <label id="name">Enter Name:</label>
+        <input type="text" onChange={hangleFormData} htmlFor="name" name="name"/>
+        <button type="submit" onClick={handleNext}>Next</button>
+        </div>
+        :
+        null
+        }
+
+        { currentPage === 2 ? 
+        <div>
+        <label id="age">Enter age:</label>
+        <input type="text" onChange={hangleFormData} htmlFor="age" name="age"/>
+        <button type="submit" onClick={handleNext}>Next</button>
+        </div>
+        :
+        null
+        }
+
+        { currentPage === 3 ? 
+        <div>
+        <label id="email">Enter email:</label>
+        <input type="text" onChange={hangleFormData} htmlFor="email" name="email"/>
+        <input type="submit" value="Submit"/>
+        </div>
+        :
+        null
+        }
+
+        { currentPage === 4 ? 
+        <div>
+          <h3>Reults: </h3>
+          <p>name: {formData.name}</p>
+          <p>age: {formData.age}</p>
+          <p>email: {formData.email}</p>
+        </div>
+        :
+        null
+        }
+
+
+ 
+
+      </form>
     </div>
   );
 }
